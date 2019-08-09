98e0cf7c76a8ac1ac7f006ee14cccf80bb27e57c
Felipe Honorio <32677477+felipehonorio93@users.noreply.github.com>
2019-08-09 16:16:41 -0300

    Add files via upload

---
+++ frase.js
@@ -0,0 +1,56 @@
+$("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
+
+function fraseAleatoria() {
+
+    $("#spinner").toggle();
+
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria)
+    .fail(function(){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+        },1500);
+    })
+    .always(function(){ //sempre escondendo o spinner
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFraseAleatoria(data) {
+    var frase = $(".frase");
+    var numeroAleatorio = Math.floor(Math.random() * data.length);
+	frase.text(data[numeroAleatorio].texto);
+	atualizaTamanhoFrase();
+	atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+function buscaFrase() {
+
+    $("#spinner").toggle();
+    var fraseId = $("#frase-id").val();
+
+    var dados = {id : fraseId}; //criacao do objeto JS que guarda a id
+
+    //passando objeto como segundo parâmetro
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function(){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+        },2000);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data) {
+
+    console.log(data);
+
+    var frase = $(".frase");
+    frase.text(data.texto); //cuidado, texto com "o" no final 
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
+}
