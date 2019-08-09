cb5325c34cee0303b928ce81bcfad002e0304f6e
Felipe Honorio <32677477+felipehonorio93@users.noreply.github.com>
2019-08-09 16:21:59 -0300

    Add files via upload

--- placar.js
+++ placar.js
@@ -1,121 +1,25 @@
-$("#botao-placar").click(mostraPlacar);
-$("#botao-sync").click(sincronizaPlacar);
-function inserePlacar(){
-    var corpoTabela = $(".placar").find("tbody");
-    var usuario = $("#usuarios").val();
-    var numPalavras = $("#contador-palavras").text();
-    var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>" ;
-    
-    var linha = novaLinha(usuario,numPalavras);
-    linha.find(".botao-remover").click(removeLinha);
+var db = require('../../config/database');
 
-    corpoTabela.append(linha);
-    $(".placar").slideDown(500);
-    scrollPlacar();
-}
-
-function scrollPlacar() {
-    var posicaoPlacar = $(".placar").offset().top;
-
-        $("html, body").animate
-    (
-    {
-        scrollTop: posicaoPlacar + "px"
-    }, 1000);
-}
+var api = {};
 
 
-function novaLinha(usuario,palavras){
-    var linha = $("<tr>");
-    var colunaUsuario = $("<td>").text(usuario);
-    var colunaPalavras = $("<td>").text(palavras);
-    var colunaRemover = $("<td>");
+api.lista = function(req, res) {
 
-    var link = $("<a>").attr("href","#").addClass("botao-remover");
-    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
-
-    // Icone dentro do <a>
-    link.append(icone);
-
-    // <a> dentro do <td>
-    colunaRemover.append(link);
-
-    // Os três <td> dentro do <tr>
-    linha.append(colunaUsuario); 
-    linha.append(colunaPalavras);
-    linha.append(colunaRemover);
-
-        return linha;
-}
-
-function removeLinha() {
-    event.preventDefault();
-    var linha = $(this).parent().parent();
-
-    linha.fadeOut(1000);
-    setTimeout(function() {
-        linha.remove();
-    }, 1000);
-}
-
-function mostraPlacar(){
-
-    $(".placar").stop().slideToggle(600);
-}
-
-
-function sincronizaPlacar(){
-    var placar = [];
-    var linhas = $("tbody>tr");
-
-    linhas.each(function(){
-        var usuario = $(this).find("td:nth-child(1)").text();
-        var palavras = $(this).find("td:nth-child(2)").text();
-
-        var score = {
-            usuario: usuario,
-            pontos: palavras            
-        };
-
-        placar.push(score);
-
-    });
-
-    var dados = {
-        placar: placar
-    };
-
- $.post("http://localhost:3000/placar", dados , function() {
-    console.log("Placar sincronizado com sucesso");
-    $(".tooltip").tooltipster("open"); 
- }).fail(function(){
-    $(".tooltip").tooltipster("open").tooltipster("content", "Falha ao sincronizar"); 
- }).always(function(){ //novo
-    setTimeout(function() {
-    $(".tooltip").tooltipster("close"); 
-   }, 1200);
-
+    db.find({}).exec(function(err, doc) {
+      if (err) return console.log(err);
+      res.json(doc);
   });
+};
 
-}
+api.insere = function(req, res){
+    db.remove({}, { multi: true }, function (err, numRemoved) {
+    });
 
-
-function atualizaPlacar(){
-    $.get("http://localhost:3000/placar",function(data){
-        $(data).each(function(){
-            var linha = novaLinha(this.usuario, this.pontos);
-            linha.find(".botao-remover").click(removeLinha);
-            $("tbody").append(linha);
-        });
+    db.insert(req.body.placar, function(err, newDoc) {
+        if(err) return console.log(err);
+        console.log('Sincronizado com sucesso: ' + newDoc._id);
+        res.json({});
     });
 }
 
-
-
-
-
-
-
-
-
-
+module.exports = api;
